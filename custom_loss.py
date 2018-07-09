36646722a3559bd279080989954e7cc9374a03fe
Cassandra Carr <cassandracarr@cassandrasmbp2.lan>
2018-07-09 18:10:25 -0400

    Changes to custom_loss

--- custom_loss.py
+++ custom_loss.py
@@ -44,79 +44,156 @@ TRUE_BOX_BUFFER  = 50
 
 
 def custom_loss(y_true, y_pred):
-    ### Adjust prediction
-    # adjust x and y      
-    pred_box_xy = tf.sigmoid(y_pred[:,:,:,:,:2])
+    mask_shape = tf.shape(y_true)[:4]
     
-    # adjust w and h
-    pred_box_wh = tf.exp(y_pred[:,:,:,:,2:4]) * np.reshape(ANCHORS, [1,1,1,BOX,2])
-    pred_box_wh = tf.sqrt(pred_box_wh / np.reshape([float(GRID_W), float(GRID_H)], [1,1,1,1,2]))
+    cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(GRID_W), [GRID_H]), (1, GRID_H, GRID_W, 1, 1)))
+    cell_y = tf.transpose(cell_x, (0,2,1,3,4))
+
+    cell_grid = tf.tile(tf.concat([cell_x,cell_y], -1), [BATCH_SIZE, 1, 1, 5, 1])
     
-    # adjust confidence
-    pred_box_conf = tf.expand_dims(tf.sigmoid(y_pred[:, :, :, :, 4]), -1)
+    coord_mask = tf.zeros(mask_shape)
+    conf_mask  = tf.zeros(mask_shape)
+    class_mask = tf.zeros(mask_shape)
     
-    # adjust probability
-    pred_box_prob = tf.nn.softmax(y_pred[:, :, :, :, 5:])
+    seen = tf.Variable(0.)
+    total_recall = tf.Variable(0.)
     
-    y_pred = tf.concat([pred_box_xy, pred_box_wh, pred_box_conf, pred_box_prob], 4)
-    print("Y_pred shape: {}".format(y_pred.shape))
+    """
+    Adjust prediction
+    """
+    ### adjust x and y      
+    pred_box_xy = tf.sigmoid(y_pred[..., :2]) + cell_grid
     
-    ### Adjust ground truth
-    # adjust x and y
-    center_xy = .5*(y_true[:,:,:,:,0:2] + y_true[:,:,:,:,2:4])
-    center_xy = center_xy / np.reshape([(float(NORM_W)/GRID_W), (float(NORM_H)/GRID_H)], [1,1,1,1,2])
-    true_box_xy = center_xy - tf.floor(center_xy)
+    ### adjust w and h
+    pred_box_wh = tf.exp(y_pred[..., 2:4]) * np.reshape(ANCHORS, [1,1,1,BOX,2])
     
-    # adjust w and h
-    true_box_wh = (y_true[:,:,:,:,2:4] - y_true[:,:,:,:,0:2])
-    true_box_wh = tf.sqrt(true_box_wh / np.reshape([float(NORM_W), float(NORM_H)], [1,1,1,1,2]))
+    ### adjust confidence
+    pred_box_conf = tf.sigmoid(y_pred[..., 4])
     
-    # adjust confidence
-    pred_tem_wh = tf.pow(pred_box_wh, 2) * np.reshape([GRID_W, GRID_H], [1,1,1,1,2])
-    pred_box_area = pred_tem_wh[:,:,:,:,0] * pred_tem_wh[:,:,:,:,1]
-    pred_box_ul = pred_box_xy - 0.5 * pred_tem_wh
-    pred_box_bd = pred_box_xy + 0.5 * pred_tem_wh
+    ### adjust class probabilities
+    pred_box_class = y_pred[..., 5:]
     
-    true_tem_wh = tf.pow(true_box_wh, 2) * np.reshape([GRID_W, GRID_H], [1,1,1,1,2])
-    true_box_area = true_tem_wh[:,:,:,:,0] * true_tem_wh[:,:,:,:,1]
-    true_box_ul = true_box_xy - 0.5 * true_tem_wh
-    true_box_bd = true_box_xy + 0.5 * true_tem_wh
+    """
+    Adjust ground truth
+    """
+    ### adjust x and y
+    true_box_xy = y_true[..., 0:2] # relative position to the containing cell
     
-    intersect_ul = tf.maximum(pred_box_ul, true_box_ul) 
-    intersect_br = tf.minimum(pred_box_bd, true_box_bd)
-    intersect_wh = intersect_br - intersect_ul
-    intersect_wh = tf.maximum(intersect_wh, 0.0)
-    intersect_area = intersect_wh[:,:,:,:,0] * intersect_wh[:,:,:,:,1]
+    ### adjust w and h
+    true_box_wh = y_true[..., 2:4] # number of cells accross, horizontally and vertically
     
-    iou = tf.truediv(intersect_area, true_box_area + pred_box_area - intersect_area)
-    best_box = tf.equal(iou, tf.reduce_max(iou, [3], True)) 
-    best_box = tf.to_float(best_box)
-    true_box_conf = tf.expand_dims(best_box * y_true[:,:,:,:,4], -1)
+    ### adjust confidence
+    true_wh_half = true_box_wh / 2.
+    true_mins    = true_box_xy - true_wh_half
+    true_maxes   = true_box_xy + true_wh_half
     
-    # adjust confidence
-    true_box_prob = y_true[:,:,:,:,5:]
+    pred_wh_half = pred_box_wh / 2.
+    pred_mins    = pred_box_xy - pred_wh_half
+    pred_maxes   = pred_box_xy + pred_wh_half       
     
-    y_true = tf.concat([true_box_xy, true_box_wh, true_box_conf, true_box_prob], 4)
-    print("Y_true shape: {}".format(y_true.shape))
-    #y_true = tf.Print(y_true, [true_box_wh], message='DEBUG', summarize=30000)    
+    intersect_mins  = tf.maximum(pred_mins,  true_mins)
+    intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+    intersect_wh    = tf.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
     
-    ### Compute the weights
-    weight_coor = tf.concat(4 * [true_box_conf], 4)
-    weight_coor = SCALE_COOR * weight_coor
+    true_areas = true_box_wh[..., 0] * true_box_wh[..., 1]
+    pred_areas = pred_box_wh[..., 0] * pred_box_wh[..., 1]
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores  = tf.truediv(intersect_areas, union_areas)
     
-    weight_conf = SCALE_NOOB * (1. - true_box_conf) + SCALE_CONF * true_box_conf
+    true_box_conf = iou_scores * y_true[..., 4]
     
-    weight_prob = tf.concat(CLASS * [true_box_conf], 4) 
-    weight_prob = SCALE_PROB * weight_prob 
+    ### adjust class probabilities
+    true_box_class = tf.argmax(y_true[..., 5:], -1)
     
-    weight = tf.concat([weight_coor, weight_conf, weight_prob], 4)
-    print("Weight shape: {}".format(weight.shape))
+    """
+    Determine the masks
+    """
+    ### coordinate mask: simply the position of the ground truth boxes (the predictors)
+    coord_mask = tf.expand_dims(y_true[..., 4], axis=-1) * COORD_SCALE
     
-    ### Finalize the loss
-    loss = tf.pow(y_pred - y_true, 2)
-    loss = loss * weight
-    loss = tf.reshape(loss, [-1, GRID_W*GRID_H*BOX*(4 + 1 + CLASS)])
-    loss = tf.reduce_sum(loss, 1)
-    loss = .5 * tf.reduce_mean(loss)
+    ### confidence mask: penelize predictors + penalize boxes with low IOU
+    # penalize the confidence of the boxes, which have IOU with some ground truth box < 0.6
+    true_xy = true_boxes[..., 0:2]
+    true_wh = true_boxes[..., 2:4]
+    
+    true_wh_half = true_wh / 2.
+    true_mins    = true_xy - true_wh_half
+    true_maxes   = true_xy + true_wh_half
+    
+    pred_xy = tf.expand_dims(pred_box_xy, 4)
+    pred_wh = tf.expand_dims(pred_box_wh, 4)
+    
+    pred_wh_half = pred_wh / 2.
+    pred_mins    = pred_xy - pred_wh_half
+    pred_maxes   = pred_xy + pred_wh_half    
+    
+    intersect_mins  = tf.maximum(pred_mins,  true_mins)
+    intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+    intersect_wh    = tf.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+    
+    true_areas = true_wh[..., 0] * true_wh[..., 1]
+    pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores  = tf.truediv(intersect_areas, union_areas)
+
+    best_ious = tf.reduce_max(iou_scores, axis=4)
+    conf_mask = conf_mask + tf.to_float(best_ious < 0.6) * (1 - y_true[..., 4]) * NO_OBJECT_SCALE
+    
+    # penalize the confidence of the boxes, which are reponsible for corresponding ground truth box
+    conf_mask = conf_mask + y_true[..., 4] * OBJECT_SCALE
+    
+    ### class mask: simply the position of the ground truth boxes (the predictors)
+    class_mask = y_true[..., 4] * tf.gather(CLASS_WEIGHTS, true_box_class) * CLASS_SCALE       
+    
+    """
+    Warm-up training
+    """
+    no_boxes_mask = tf.to_float(coord_mask < COORD_SCALE/2.)
+    seen = tf.assign_add(seen, 1.)
+    
+    true_box_xy, true_box_wh, coord_mask = tf.cond(tf.less(seen, WARM_UP_BATCHES), 
+                          lambda: [true_box_xy + (0.5 + cell_grid) * no_boxes_mask, 
+                                   true_box_wh + tf.ones_like(true_box_wh) * np.reshape(ANCHORS, [1,1,1,BOX,2]) * no_boxes_mask, 
+                                   tf.ones_like(coord_mask)],
+                          lambda: [true_box_xy, 
+                                   true_box_wh,
+                                   coord_mask])
+    
+    """
+    Finalize the loss
+    """
+    nb_coord_box = tf.reduce_sum(tf.to_float(coord_mask > 0.0))
+    nb_conf_box  = tf.reduce_sum(tf.to_float(conf_mask  > 0.0))
+    nb_class_box = tf.reduce_sum(tf.to_float(class_mask > 0.0))
+    
+    loss_xy    = tf.reduce_sum(tf.square(true_box_xy-pred_box_xy)     * coord_mask) / (nb_coord_box + 1e-6) / 2.
+    loss_wh    = tf.reduce_sum(tf.square(true_box_wh-pred_box_wh)     * coord_mask) / (nb_coord_box + 1e-6) / 2.
+    loss_conf  = tf.reduce_sum(tf.square(true_box_conf-pred_box_conf) * conf_mask)  / (nb_conf_box  + 1e-6) / 2.
+    loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class, logits=pred_box_class)
+    loss_class = tf.reduce_sum(loss_class * class_mask) / (nb_class_box + 1e-6)
+    
+    loss = loss_xy + loss_wh + loss_conf + loss_class
+    
+    nb_true_box = tf.reduce_sum(y_true[..., 4])
+    nb_pred_box = tf.reduce_sum(tf.to_float(true_box_conf > 0.5) * tf.to_float(pred_box_conf > 0.3))
+
+    """
+    Debugging code
+    """    
+    current_recall = nb_pred_box/(nb_true_box + 1e-6)
+    total_recall = tf.assign_add(total_recall, current_recall) 
+
+    loss = tf.Print(loss, [tf.zeros((1))], message='Dummy Line \t', summarize=1000)
+    loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
+    loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
+    loss = tf.Print(loss, [loss_conf], message='Loss Conf \t', summarize=1000)
+    loss = tf.Print(loss, [loss_class], message='Loss Class \t', summarize=1000)
+    loss = tf.Print(loss, [loss], message='Total Loss \t', summarize=1000)
+    loss = tf.Print(loss, [current_recall], message='Current Recall \t', summarize=1000)
+    loss = tf.Print(loss, [total_recall/seen], message='Average Recall \t', summarize=1000)
     
     return loss
