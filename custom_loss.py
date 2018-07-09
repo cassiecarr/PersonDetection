3efc067509c2f8dd36501cad39d6ab593462e1b0
Cassandra Carr <cassandracarr@cassandrasmbp2.lan>
2018-07-09 18:02:37 -0400

    Add initalizations to custom loss

--- custom_loss.py
+++ custom_loss.py
@@ -1,4 +1,24 @@
-
+# intialization
+from keras.models import Sequential, Model
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.layers.merge import concatenate
+from keras import initializers
+import matplotlib.pyplot as plt
+import keras.backend as K
+import tensorflow as tf
+import imgaug as ia
+from tqdm import tqdm
+from imgaug import augmenters as iaa
+import numpy as np
+import pickle
+import os, cv2
+from preprocessing import parse_annotation, BatchGenerator
+from utils import WeightReader, decode_netout, draw_boxes
+from keras.models import load_model
+from custom_loss import custom_loss
 
 def custom_loss(y_true, y_pred):
     ### Adjust prediction
