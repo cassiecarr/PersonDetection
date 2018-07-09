2dd1c46dc12270073582a7eca23403a1f5ccb7ee
Cassandra Carr <cassandracarr@cassandrasmbp2.lan>
2018-07-09 18:06:06 -0400

    Add variable initialization

--- custom_loss.py
+++ custom_loss.py
@@ -19,6 +19,30 @@ from preprocessing import parse_annotation, BatchGenerator
 from utils import WeightReader, decode_netout, draw_boxes
 from keras.models import load_model
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# define parameters 
+LABELS = ['person']
+IMG_H, IMG_W = 416, 416
+GRID_H, GRID_W = 13 , 13
+BOX = 5
+CLASS = len(LABELS)
+CLASS_WEIGHTS = np.ones(CLASS, dtype='float32')
+OBJ_THRESHOLD    = 0.3
+NMS_THRESHOLD    = 0.3
+ANCHORS          = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
+
+NO_OBJECT_SCALE  = 1.0
+OBJECT_SCALE     = 5.0
+COORD_SCALE      = 1.0
+CLASS_SCALE      = 1.0
+
+BATCH_SIZE       = 16
+WARM_UP_BATCHES  = 0
+TRUE_BOX_BUFFER  = 50
+
+
 def custom_loss(y_true, y_pred):
     ### Adjust prediction
     # adjust x and y      
